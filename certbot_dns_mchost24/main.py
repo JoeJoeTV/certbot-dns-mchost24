d0d36dc5cd1ea1385de90b959326603a7887ce4b
Johannes Schöneberger <hannes1033@gmail.com>
2024-06-16 00:46:29 +0200

    Initial implementation

---
+++ certbot_dns_mchost24/main.py
@@ -0,0 +1,130 @@
+import logging
+
+from certbot import errors
+from certbot.plugins import dns_common
+
+from mchost24.api import DomainRecordType, MCHost24API, MCHost24APIError
+
+logger = logging.getLogger(__name__)
+
+class Authenticator(dns_common.DNSAuthenticator):
+    """DNS Authenticator for MCHost24
+    
+    Uses the MCHost24 API to fullfill a dns-01 challenge
+    """
+    
+    description = 'Obtain certificates using a DNS TXT record for MCHost24.'
+    
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
+    
+    @classmethod
+    def add_parser_arguments(cls, add):
+        super(Authenticator, cls).add_parser_arguments(
+            add, default_propagation_seconds = 120
+        )
+        add("credentials", help="MCHost24 credentials INI file.")
+    
+    def more_info(self):
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 "
+            "challenge using the MCHost24 API."
+        )
+    
+    def _setup_credentials(self):
+        self.credentials = self._configure_credentials(
+            "credentials",
+            "MCHost24 API credentials INI file. Only API token "
+            "authentication is supported",
+            {"api-token": "API token for the MCHost24 API"},
+        )
+    
+    def _perform(self, domain, validation_name, validation):
+        MCH24APIWrapper(self.credentials.conf("api-token")).add_txt_record(
+            domain,
+            validation_name.replace("." + domain, ""),
+            validation
+        )
+    
+    def _cleanup(self, domain, validation_name, validation):
+        MCH24APIWrapper(self.credentials.conf("api-token")).del_txt_record(
+            domain,
+            validation_name.replace("." + domain, ""),
+            validation
+        )
+
+class MCH24APIWrapper(object):
+    
+    def __init__(self, api_token: str):
+        self.api_client = MCHost24API(token = api_token)
+    
+    def _get_domains(self):
+        try:
+            res = self.api_client.get_domains()
+        
+            if not res.success:
+                raise errors.PluginError(f"Couldn't get list of domains from API: {res.message}")
+            
+            return res.data
+        except MCHost24APIError as e:
+            raise errors.PluginError(f"Error while getting list of domains.") from e
+    
+    def _get_dns_records(self, domain_id: int):
+        try:
+            res = self.api_client.get_domain_info(domain_id)
+        
+            if not res.success:
+                raise errors.PluginError(f"Couldn't get domain info from API: {res.message}")
+            
+            return res.data.records
+        except MCHost24APIError as e:
+            raise errors.PluginError(f"Error while getting list of dns records for domain.") from e
+    
+    def _get_domain_id(self, domain: str):
+        components = domain.split('.')
+        
+        if len(components) < 2:
+            raise errors.PluginError(f"Domain has too few components: {len(components)}")
+        
+        id = None
+        for d in self._get_domains():
+            if (components[-1] == d.tld) and (components[-2] == d.sld):
+                id = d.id
+        
+        if id is None:
+            raise errors.PluginError(f"Unknown domain: {components[-2]}.{components[-1]}")
+        
+        return id
+    
+    def add_txt_record(self, domain: str, record_name: str, record_content: str):
+        domain_id = self._get_domain_id(domain)
+        
+        try:
+            res = self.api_client.create_domain_dns_record(domain_id, record_name, DomainRecordType.TXT, record_content)
+            
+            if not res:
+                raise errors.PluginError(f"Creating DNS record failed: {res.message}")
+        except MCHost24APIError as e:
+            raise errors.PluginError("Error while creating DNS record.") from e
+    
+    def del_txt_record(self, domain: str, record_name: str, record_content: str):
+        domain_id = self._get_domain_id(domain)
+        records = self._get_dns_records(domain_id)
+        
+        id = None
+        for r in records:
+            if r.sld == record_name:
+                id = r.id
+        
+        if id is None:
+            # No record found to delete
+            return
+        
+        try:
+            res = self.api_client.delete_domain_dns_record(domain_id, id)
+        
+            if not res.success:
+                logger.error(f"Couldn't delete DNS record: {res.message}")
+        except MCHost24APIError as e:
+            logger.error(f"{e.__class__.__name__}:{e.message}")
---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from setuptools import find_packages
+from os import path
+
+install_requirements = [
+    "acme>=2.9.0",
+    "certbot>=2.9.0",
+    "setuptools",
+    "mchost24",
+]
+
+setup_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(setup_dir, "README.md")) as file:
+    long_description = file.read()
+
+setup(
+    name="certbot-dns-mchost24",
+    version="1.0.0",
+    description="Certbot DNS Authenticator plugin for MCHost24",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoeJoeTV/certbot-dns-mchost24",
+    author="JoeJoeTV",
+    author_email="joejoetv@joejoetv.de",
+    license="GNU General Public License v3.0",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        "certbot.plugins": [
+            "dns-mchost24 = certbot_dns_mchost24.main:Authenticator"
+        ]
+    },
+)
