83a0450557977aacb467c71cd196590118c87f98
Johannes Schöneberger <hannes1033@gmail.com>
2024-06-16 02:51:57 +0200

    Lower propagation wait time

--- certbot_dns_mchost24/main.py
+++ certbot_dns_mchost24/main.py
@@ -22,7 +22,7 @@ class Authenticator(dns_common.DNSAuthenticator):
     @classmethod
     def add_parser_arguments(cls, add):
         super(Authenticator, cls).add_parser_arguments(
-            add, default_propagation_seconds = 120
+            add, default_propagation_seconds = 60
         )
         add("credentials", help="MCHost24 credentials INI file.")
     
